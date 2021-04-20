539be816ea6af32476717d58d5b6d88fbb1d3bc2
Evan-Leon <evan.leon.j@gmail.com>
2021-04-20 16:00:14 -0400

    Added game functions

--- src/game.js
+++ src/game.js
@@ -1,17 +1,42 @@
+const Asteroid = require("./asteroid");
 
+const DIM_X = 500;
+const DIM_Y = 500;
+const NUM_ASTEROIDS= 20;
 
-Game.prototype.step = function(){
+function Game(){
+    this.asteroids = [];
+    this.addAsteroids();
 
 }
 
-Game.prototype.move = function(){
-
+Game.prototype.addAsteroids = function (){
+    for (let i = 1; i < NUM_ASTEROIDS; i++){
+        this.asteroids.push(new Asteroid());
+    }
 }
 
-Game.prototype.checkCollisions = function(){
-
+Game.prototype.moveObjects = function (){
+    asteroids.forEach(function (asteroid){
+        asteroid.move();
+    });
 }
 
 Game.prototype.draw(ctx) = function(){
-
+    ctx.clearRect();
+    asteroids.forEach(function (asteroid){
+        asteroid.draw(ctx);
+    });
 }
+
+// Game.prototype.step = function(){
+
+// }
+
+// Game.prototype.move = function(){
+
+// }
+
+// Game.prototype.checkCollisions = function(){
+
+// }
